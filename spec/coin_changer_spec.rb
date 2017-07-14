cbb95055400a7f2de33e251d21d65a238c701812
esanmiguelc <esanmiguel@gmail.com>
2017-07-14 10:39:58 -0500

    Coin Changer implementation that handles up to 4 cents
     - Handles a single cent
     - Handles up to 4 cents

---
+++ spec/coin_changer_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+class CoinChanger
+  def self.to_change(num)
+    result = []
+    (0...num).each do |n|
+      result << 1
+    end
+    result
+  end
+end
+
+describe "coin changer" do
+  it "changes one cent" do
+    expect(CoinChanger.to_change(1)).to eq([1])
+  end
+
+  it "changes two cents" do
+    expect(CoinChanger.to_change(2)).to eq([1, 1])
+  end
+end
